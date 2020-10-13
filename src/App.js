9e94ec8af55431607659264144d4e86d1ac3d0fd
White Joan <joannnabiala@gmail.com>
2020-10-13 11:59:28 +0200

    Example task displaying added. Some basic styling added.

--- src/App.js
+++ src/App.js
@@ -28,33 +28,60 @@ class App extends React.Component {
     console.log('fetching...! ')
 
     axios.get('http://127.0.0.1:8000/api/task-list/')
-     .then((response)=>{
-      console.log(response);
-      }
-    ).catch( (error)=>{
+      .then((response) => {
+          console.log(response);
+          this.setState({
+            todoList: response.data
+          })
+          console.log(this.state.todoList)
+        }
+      ).catch((error) => {
         console.log(error);
       }
     )
   }
 
   render() {
+
+    var tasks = this.state.todoList;
+
     return (
       <div className="container">
-        <div id="form-wrapper">
-          <form id="form">
-            <div className="flex-wrapper">
-              <div style={{flex: 6}}>
-                <input className="form-control" id="title" name="title" placeholder="title"/>
+        <div id="task-container">
+          <div id="form-wrapper">
+            <form id="form">
+              <div className="flex-wrapper">
+                <div style={{flex: 6}}>
+                  <input className="form-control" id="title" name="title" placeholder="title"/>
+                </div>
+                <div style={{flex: 1}}>
+                  <input id="submit" className="btn btn-warning" type="submit" name="add"/>
+                </div>
               </div>
-              <div style={{flex: 1}}>
-                <input id="submit" className="btn btn-warning" type="submit" name="add"/>
-              </div>
-            </div>
-          </form>
-        </div>
+            </form>
+          </div>
 
-        <div id="list-wrapper">
+          <div id="list-wrapper">
+            {tasks.map(function (task, index) {
+              return (
+                <div key={index} className="task-wrapper flex-wrapper ">
+                  <div style={{flex: 7}}>
+                    <span>
+                      {task.title}
+                    </span>
+                  </div>
 
+                  <div style={{flex:1}}>
+                    <button className="btn btn-sm btn-outline-info">Edit</button>
+                  </div>
+
+                  <div>
+                   <button className="btn btn-sm btn-outline-dark delete">-</button>
+                  </div>
+                </div>
+              )
+            })}
+          </div>
         </div>
       </div>
     )
