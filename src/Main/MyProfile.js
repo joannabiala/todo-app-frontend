53de1a7246c24c9d0ad1e88586f2fb8c51b5422c
White Joan <joannnabiala@gmail.com>
2020-11-12 11:30:47 +0100

    Frontend: Refactored into hooks - MyProfile.js

--- src/Main/MyProfile.js
+++ src/Main/MyProfile.js
@@ -1,106 +1,78 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import axios from 'axios';
-import '../App.css';
 import {Redirect} from "react-router-dom";
+
 import TaskComponent from "./TaskComponent";
 import PageTemplateComponent from "./PageTemplateComponent";
+import '../App.css';
 
 
-export default class MyProfile extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      todoList: [],
-      list_name: '',
+const MyProfile = () => {
 
-      activeItem: {
-        id: null,
-        title: '',
-        completed: false,
-        list: '',
-      },
+  const [todoList, setTodoList] = useState([])
+  const [list_name, setList_name] = useState('')
+  const [activeItem, setActiveItem] = useState({
+    id: null,
+    title: '',
+    completed: false,
+    list: ''
+  })
 
-      editing: false,
-      redirectToMain: false,
-      searchField: '',
+  const [editing, setEditing] = useState(false)
+  const [redirectToMain, setRedirectToMain] = useState(false)
+  const [addingList, setAddingList] = useState(false)
+  const [isLoadingContent, setIsLoadingContent] = useState(true)
 
-    }
 
-    this.fetchTasks = this.fetchTasks.bind(this);
-    this.handleSubmitListName = this.handleSubmitListName.bind(this);
-    this.handleSubmitTask = this.handleSubmitTask.bind(this);
-    this.handleDelete = this.handleDelete.bind(this);
-    this.handleUpdate = this.handleUpdate.bind(this);
-    this.handleComplete = this.handleComplete.bind(this);
-    this.handleChangeListName = this.handleChangeListName.bind(this);
-    this.handleChange = this.handleChange.bind(this);
-    this.handleChangeListID = this.handleChangeListID.bind(this);
-    this.handleDeleteList = this.handleDeleteList.bind(this);
+  const handleChangeListID = (event) => {
+    const updatedActiveItem = {...activeItem}
+    updatedActiveItem.list = event.target.value
+
+    setActiveItem(updatedActiveItem)
   }
 
 
-  handleChangeListName(event) {
-    this.setState({
-      list_name: event.target.value,
-    });
-  }
+  const handleUpdate = (task) => {
+    setEditing(true)
 
-
-  handleChangeListID(event) {
-    const activeItem = this.state.activeItem
-    activeItem.list = event.target.value
-
-    this.setState({
-      activeItem: activeItem
-    });
-  }
-
-
-  handleUpdate(task) {
-    this.setState({
-      editing: true
-    })
-
-    if (this.state.editing === true) {
-      this.setState({
-        activeItem: {
-          title: task.title,
-          id: task.id,
-          completed: task.completed
-        }
+    if (editing === true) {
+      setActiveItem({
+        title: task.title,
+        id: task.id,
+        completed: task.completed
       })
     }
   }
 
 
-  handleChange(event) {
+  const handleChange = (event) => {
     let name = event.target.name
     let value = event.target.value
 
     console.log('name: ', name)
     console.log('value: ', value)
 
-    this.setState({
-      activeItem: {
-        ...this.state.activeItem,
-        title: value,
-      }
+    setActiveItem({
+      ...activeItem,
+      title: value,
     })
   }
 
 
-  componentDidMount() {
-    this.fetchTasks()
-  }
+  useEffect(() => {
+    fetchTasks()
+  }, [])
 
-  fetchTasks() {
+
+  const fetchTasks = () => {
     axios.get('http://127.0.0.1:8000/api/list/')
       .then((response) => {
         console.log(response)
-        this.setState({
-          todoList: response.data,
-        })
-        console.log(this.state.todoList)
+
+        setTodoList(response.data)
+        setIsLoadingContent(false)
+
+        console.log(todoList)
       })
       .catch((error) => {
         console.log(error)
@@ -108,35 +80,31 @@ export default class MyProfile extends React.Component {
   }
 
 
-  handleSubmitTask(event) {
+  const handleSubmitTask = (event) => {
     event.preventDefault();
 
-    if (this.state.editing === true) {
-      axios.put('http://127.0.0.1:8000/api/tasks/' + this.state.activeItem.id + '/', this.state.activeItem).then(
+    if (editing === true) {
+      axios.put('http://127.0.0.1:8000/api/tasks/' + activeItem.id + '/', activeItem).then(
         (response) => {
-          console.log(this.state.list)
-          this.fetchTasks()
+          console.log(activeItem.list)
+          fetchTasks()
           console.log(response);
-          this.setState({
-            editing: false
-          })
+          setEditing(false)
         }
       ).catch((error) => {
         console.log(error)
       })
     } else {
-      axios.post('http://127.0.0.1:8000/api/tasks/', this.state.activeItem)
+      axios.post('http://127.0.0.1:8000/api/tasks/', activeItem)
         .then((response) => {
-          this.setState({
-              activeItem: {
-                id: null,
-                title: '',
-                completed: false,
-              }
-            }
-          )
+          setActiveItem({
+            id: null,
+            title: '',
+            completed: false,
+          })
+
           console.log(response);
-          this.fetchTasks()
+          fetchTasks()
         })
         .catch((error) => {
           console.log(error);
@@ -145,7 +113,7 @@ export default class MyProfile extends React.Component {
   }
 
 
-  handleComplete(task) {
+  const handleComplete = (task) => {
 
     task.completed = !task.completed
     console.log(task.title)
@@ -153,10 +121,7 @@ export default class MyProfile extends React.Component {
 
     axios.patch('http://127.0.0.1:8000/api/tasks/' + task.id + '/', task).then(
       (response) => {
-        this.setState({
-            completed: task.completed
-          }
-        )
+        setActiveItem(task.completed)
         console.log(response)
       }
     ).catch((error) => {
@@ -165,14 +130,14 @@ export default class MyProfile extends React.Component {
   }
 
 
-  handleSubmitListName(event) {
+  const handleSubmitListName = (event) => {
     event.preventDefault();
     const data = {
-      list_name: this.state.list_name
+      list_name: list_name
     }
     axios.post('http://127.0.0.1:8000/api/list/', data)
       .then((response) => {
-        this.fetchTasks()
+        fetchTasks()
         console.log(response)
       })
       .catch((error) => {
@@ -181,10 +146,10 @@ export default class MyProfile extends React.Component {
   }
 
 
-  handleDeleteList(list) {
+  const handleDeleteList = (list) => {
     axios.delete('http://127.0.0.1:8000/api/list/' + list.id + '/').then(
       (response) => {
-        this.fetchTasks()
+        fetchTasks()
         console.log(response);
       }
     ).catch((error) => {
@@ -193,10 +158,10 @@ export default class MyProfile extends React.Component {
   }
 
 
-  handleDelete(task) {
+  const handleDelete = (task) => {
     axios.delete('http://127.0.0.1:8000/api/tasks/' + task.id + '/').then(
       (response) => {
-        this.fetchTasks()
+        fetchTasks()
         console.log(response);
       }
     ).catch((error) => {
@@ -205,111 +170,146 @@ export default class MyProfile extends React.Component {
   }
 
 
-  render() {
-    let lists = this.state.todoList;
-    let self = this;
+  const addList = () => {
+    setAddingList(true)
+  }
+
+  let lists = todoList;
 
 
-    if (this.state.redirectToMain) {
-      return (<Redirect to="/main"/>)
-    }
-
-    return (
-
-      <PageTemplateComponent>
-        <div>Start creating your lists!</div>
-        <div>
-          <div className="container">
-            <div id="task-container">
-              <div id="form-wrapper">
-                <form onSubmit={this.handleSubmitListName} id="form">
-                  <div className="flex-wrapper">
-                    <div style={{flex: 6}}>
-                      <input
-                        className="form-control"
-                        id="title"
-                        name="title"
-                        placeholder="nazwa listy"
-                        onChange={this.handleChangeListName}
-                        value={this.state.list_name}
-                      />
-                    </div>
-                    <div style={{flex: 1}}>
-                      <input id="submit" className="btn btn-warning" type="submit" name="add"/>
-                    </div>
-                  </div>
-                </form>
-                <form onSubmit={this.handleSubmitTask} id="form">
-                  <div className="flex-wrapper">
-                    <div style={{flex: 6}}>
-                      <input
-                        className="form-control"
-                        id="title"
-                        name="title"
-                        placeholder="nazwa taska"
-                        onChange={this.handleChange}
-                        value={this.state.activeItem.title}
-                      />
-                      <input
-                        className="form-control"
-                        placeholder="id listy"
-                        onChange={this.handleChangeListID}
-                        value={this.state.list}
-                      />
-                    </div>
-                    <div style={{flex: 1}}>
-                      <input id="submit" className="btn btn-warning" type="submit" name="add"/>
-                    </div>
-                  </div>
-                </form>
-              </div>
+  if (redirectToMain) {
+    return (<Redirect to="/main"/>)
+  }
 
 
-              <div id="list-wrapper">
-                {lists.map((list, index) => {
-                  return (
-                    <div key={index} className="task-wrapper flex-wrapper ">
-                      <div style={{flex: 7}}>
-                        <h5>{list.list_name}</h5>
-                        <div>
-                          <button onClick={() => self.handleDeleteList(list)}
-                                  className="btn btn-sm btn-outline-dark delete">-
-                          </button>
+  return (
+
+    <PageTemplateComponent>
+      <div>
+        <div className="container">
+          <div id="task-container">
+
+            {isLoadingContent ? (<div/>) :
+              todoList.length === 0 && addingList === false ? (
+                  <button onClick={addList}>
+                    Jakiś buton
+                  </button>)
+                :
+                (<div id="form-wrapper">
+                    <form onSubmit={handleSubmitListName} id="form">
+                      <div className="flex-wrapper">
+                        <div style={{flex: 6}}>
+                          <input
+                            className="form-control"
+                            id="title"
+                            name="title"
+                            placeholder="nazwa listy"
+                            onChange={(e) => setList_name(e.target.value)}
+                            value={list_name}
+                          />
                         </div>
-                        <br/>
-                        {list.taski.map((task, index) => {
-                          return (
-                            <div key={index}>
-                              <div style={{flex: 7}}>
-                             <span onClick={() => self.handleComplete(task)}>
+                        <div style={{flex: 1}}>
+
+                          <input
+                            id="submit"
+                            className="btn btn-warning"
+                            type="submit"
+                            name="add"
+                          />
+
+                        </div>
+                      </div>
+                    </form>
+                    <form onSubmit={handleSubmitTask} id="form">
+                      <div className="flex-wrapper">
+                        <div style={{flex: 6}}>
+                          <input
+                            className="form-control"
+                            id="title"
+                            name="title"
+                            placeholder="nazwa taska"
+                            onChange={handleChange}
+                            value={activeItem.title}
+                          />
+
+                          <input
+                            className="form-control"
+                            placeholder="id listy"
+                            onChange={handleChangeListID}
+                            value={activeItem.list}
+                          />
+
+                        </div>
+                        <div style={{flex: 1}}>
+                          <input
+                            id="submit"
+                            className="btn btn-warning"
+                            type="submit"
+                            name="add"
+                          />
+                        </div>
+                      </div>
+                    </form>
+                  </div>
+                )
+
+            }
+
+            <div id="list-wrapper">
+              {lists.map((list, index) => {
+                return (
+                  <div key={index} className="task-wrapper flex-wrapper ">
+                    <div style={{flex: 7}}>
+                      <h5>{list.list_name}</h5>
+                      <div>
+                        <button
+                          onClick={() => handleDeleteList(list)}
+                          className="btn btn-sm btn-outline-dark delete"
+                        >
+                          -
+                        </button>
+                      </div>
+                      <br/>
+                      {list.taski.map((task, index) => {
+                        return (
+                          <div key={index}>
+                            <div style={{flex: 7}}>
+                             <span onClick={() => handleComplete(task)}>
                                {task.completed === false ? (<span><TaskComponent task={task}/></span>) : (
                                  <del>
                                    <TaskComponent task={task}/>
                                  </del>
                                )}
                                </span>
-                              </div>
-                              <div>
-                                <button onClick={() => self.handleDelete(task)}
-                                        className="btn btn-sm btn-outline-dark delete">-
-                                </button>
-                              </div>
-                              <div style={{flex: 1}}>
-                                <button onClick={() => self.handleUpdate(task)}
-                                        className="btn btn-sm btn-outline-info">Edit
-                                </button>
-                              </div>
-                            </div>)
-                        })}
-                      </div>
+                            </div>
+                            <div>
+                              <button
+                                onClick={() => handleDelete(task)}
+                                className="btn btn-sm btn-outline-dark delete"
+                              >
+                                -
+                              </button>
+                            </div>
+                            <div style={{flex: 1}}>
+                              <button
+                                onClick={() => handleUpdate(task)}
+                                className="btn btn-sm btn-outline-info"
+                              >
+                                Edit
+                              </button>
+                            </div>
+                          </div>)
+                      })}
                     </div>
-                  )
-                })}
-              </div>
+                  </div>
+                )
+              })}
             </div>
           </div>
         </div>
-      </PageTemplateComponent>
-    )
-  }
+      </div>
+    </PageTemplateComponent>
+  )
 }
+
+export default MyProfile
