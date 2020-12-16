2debfb4450c0e143c47c0c118cc617ee7447ddd2
White Joan <joannnabiala@gmail.com>
2020-12-16 11:14:10 +0100

    Frontend: Calendar component - added.

--- src/Components/CalendarComponent.js
+++ src/Components/CalendarComponent.js
@@ -1,8 +1,23 @@
-import React from "react";
+import React, { useState } from 'react';
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
 
 const CalendarComponent = ()=>{
+  const [value, onChange] = useState(new Date());
   return(
     <div className="col-3" id="calendarComponent">
+      <div className="row">
+      <div>
+        <Calendar
+          onChange={onChange}
+          value={value}
+        />
+      </div>
+        <div id="footer">
+          <div className="footer-copyright text-center py-3">© 2020 Joanna Biała
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
