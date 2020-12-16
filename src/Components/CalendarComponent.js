912f769c1e9411f0a864243894066829897843cd
White Joan <joannnabiala@gmail.com>
2020-12-16 16:34:41 +0100

    Frontend: Sass file added for calendar component.

--- src/Components/CalendarComponent.js
+++ src/Components/CalendarComponent.js
@@ -1,6 +1,5 @@
 import React, {useState} from 'react';
 import Calendar from 'react-calendar';
-import 'react-calendar/dist/Calendar.css';
 
 const CalendarComponent = () => {
   const [value, onChange] = useState(new Date());
