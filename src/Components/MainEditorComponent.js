632e576f4df0e93f10cf903a760cb5d41abdae4f
White Joan <joannnabiala@gmail.com>
2020-12-18 14:54:06 +0100

    Frontend: displaying task form toggler.

--- src/Components/MainEditorComponent.js
+++ src/Components/MainEditorComponent.js
@@ -4,7 +4,7 @@ import {Redirect} from "react-router-dom";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faEdit, faPlusSquare, faTrashAlt} from "@fortawesome/free-solid-svg-icons";
 
-const MainEditorComponent = ({index, list}) => {
+const MainEditorComponent = ({list}) => {
   const [todoList, setTodoList] = useState([])
   const [list_name, setList_name] = useState('')
   const [activeItem, setActiveItem] = useState({
@@ -13,18 +13,15 @@ const MainEditorComponent = ({index, list}) => {
     completed: false,
     list: ''
   })
+
   const [editing, setEditing] = useState(false)
   const [redirectToMain, setRedirectToMain] = useState(false)
-  const [addingList, setAddingList] = useState(false)
   const [isLoadingContent, setIsLoadingContent] = useState(true)
   const [isToggled, setToggled] = useState(false);
-  const toggleTrueFalse = () => setToggled(!isToggled);
+  const [isToggledTaskForm, setToggledTaskForm] = useState(false);
 
-  const handleChangeListID = (event) => {
-    const updatedActiveItem = {...activeItem}
-    updatedActiveItem.list = event.target.value
-    setActiveItem(updatedActiveItem)
-  }
+  const toggleTrueFalse = () => setToggled(!isToggled);
+  const toggleTaskForm = () => setToggledTaskForm(!isToggledTaskForm);
 
 
   const handleChange = (event) => {
@@ -37,13 +34,124 @@ const MainEditorComponent = ({index, list}) => {
     setActiveItem({
       ...activeItem,
       title: value,
+      list: list.id
+    })
+  }
+
+  const refreshPage = () => {
+    window.location.reload(false);
+  }
+
+
+  const handleDeleteList = (list) => {
+    axios.delete('http://127.0.0.1:8000/api/list/' + list.id + '/').then(
+      (response) => {
+        fetchTasks()
+        console.log(response);
+      }
+    ).catch((error) => {
+      console.log(error)
     })
   }
 
 
-  useEffect(() => {
-    fetchTasks()
-  }, [])
+  const handleSubmitListName = (event) => {
+    event.preventDefault();
+    const data = {
+      list_name: list_name
+    }
+    axios.post('http://127.0.0.1:8000/api/list/', data)
+      .then((response) => {
+        fetchTasks()
+        refreshPage()
+        console.log(response)
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }
+
+
+  const renderClickedList = (list) => {
+    return (
+      <div onClick={console.log("dupa" + list.id)} className="list-group col-12">
+        <p id="listWrapper" href="#" className="col-12 d-flex flex-row list-group-item flex-column">
+          <h5 id="listName">
+            {list.list_name}
+          </h5>
+          <div onClick={() => toggleTaskForm()}>
+            <FontAwesomeIcon icon={faPlusSquare}/>
+          </div>
+
+          {isToggledTaskForm ? showTaskForm() : <div> Add new task</div>}
+
+          {list.taski.map((task) => {
+            return (
+              <div id="tasksWrapper">
+                <div id="taskTitle">
+                <span onClick={() => handleComplete(task)}>
+                  {task.completed === false
+                    ?
+                    (<span>
+                      {task.title}
+                      {task.completed}
+                    </span>)
+                    :
+                    (<del>
+                        <p>
+                          {task.title}
+                        </p>
+                        {task.completed}
+                      </del>
+                    )}
+                </span>
+                </div>
+                <div>
+                  <FontAwesomeIcon onClick={() => handleDelete(task)} icon={faTrashAlt}/>
+                </div>
+                <div>
+                  <FontAwesomeIcon onClick={() => handleUpdate(task)} icon={faEdit}/>
+                </div>
+                <hr/>
+              </div>
+            )
+          })
+          }
+        </p>
+      </div>
+    )
+  }
+
+
+  const showListForm = () => {
+    return (
+      <div>
+        <form id="form" className="row">
+          <div className="col-12">
+            <input
+              id="title"
+              className="form-control"
+              name="title"
+              placeholder="lists name"
+              onChange={(e) => setList_name(e.target.value)}
+              value={list_name}
+            />
+          </div>
+          <div className="col-5">
+            <button
+              onClick={handleSubmitListName}
+              id="submit"
+              className="btn btn-primary"
+              type="submit"
+              name="add"
+            >
+              Create list
+            </button>
+          </div>
+        </form>
+      </div>
+    )
+  }
 
 
   const fetchTasks = () => {
@@ -60,72 +168,27 @@ const MainEditorComponent = ({index, list}) => {
   }
 
 
+  useEffect(() => {
+    fetchTasks()
+  }, [])
+
+
   const handleSubmitTask = (event) => {
     event.preventDefault();
-    if (editing === true) {
-      axios.put('http://127.0.0.1:8000/api/tasks/' + activeItem.id + '/', activeItem).then(
-        (response) => {
-          console.log(activeItem.list)
-          fetchTasks()
-          console.log(response);
-          setEditing(false)
-        }
-      ).catch((error) => {
-        console.log(error)
-      })
-    } else {
-      axios.post('http://127.0.0.1:8000/api/tasks/', activeItem)
-        .then((response) => {
-          setActiveItem({
-            id: null,
-            title: '',
-            completed: false,
-          })
-          console.log(response);
-          fetchTasks()
-        })
-        .catch((error) => {
-          console.log(error);
-        })
-    }
-  }
-
-
-  const handleSubmitListName = (event) => {
-    event.preventDefault();
-    const data = {
-      list_name: list_name
-    }
-    axios.post('http://127.0.0.1:8000/api/list/', data)
+    axios.post('http://127.0.0.1:8000/api/tasks/', activeItem)
       .then((response) => {
+        console.log(response);
+        refreshPage()
         fetchTasks()
-        console.log(response)
       })
       .catch((error) => {
-        console.log(error)
+        console.log(error);
       })
-  }
-
-
-  const addList = () => {
-    setAddingList(true)
   }
 
 
   if (redirectToMain) {
     return (<Redirect to="/main"/>)
-  }
-
-
-  const handleDeleteList = (list) => {
-    axios.delete('http://127.0.0.1:8000/api/list/' + list.id + '/').then(
-      (response) => {
-        fetchTasks()
-        console.log(response);
-      }
-    ).catch((error) => {
-      console.log(error)
-    })
   }
 
 
@@ -170,113 +233,38 @@ const MainEditorComponent = ({index, list}) => {
   }
 
 
-  const renderClickedList = (list) => {
+  const showTaskForm = () => {
     return (
-      <div className="list-group col-12">
-        <p id="listWrapper" href="#" className="col-12 d-flex flex-row list-group-item flex-column">
-          <h5 id="listName">
-            {list.list_name}
-          </h5>
-          {list.taski.map((task) => {
-            return (
-              <div id="tasksWrapper">
-                <div id="taskTitle">
-                <span onClick={() => handleComplete(task)}>
-                  {task.completed === false
-                    ?
-                    (<span>
-                      {task.title}
-                      {task.completed}
-                    </span>)
-                    :
-                    (<del>
-                        <p>
-                          {task.title}
-                        </p>
-                        {task.completed}
-                      </del>
-                    )}
-                </span>
-                </div>
-                <div>
-                  <FontAwesomeIcon onClick={() => handleDelete(task)} icon={faTrashAlt}/>
-                </div>
-                <div>
-                  <FontAwesomeIcon onClick={() => handleUpdate(task)} icon={faEdit}/>
-                </div>
-                <hr/>
-              </div>
-            )
-          })
-          }
-        </p>
-      </div>
+      <form onSubmit={handleSubmitTask} id="form">
+        <div>
+          <input
+            id="title"
+            className="form-control"
+            name="title"
+            placeholder="nazwa taska"
+            onChange={handleChange}
+            value={activeItem.title}
+          />
+          <input
+            id="submit"
+            className="form-control"
+            type="submit"
+            name="add"
+          />
+        </div>
+      </form>
     )
   }
 
-
-  const showForm = () => {
-    return (
-      <div className="col-12">
-        <form id="form">
-          <div>
-            <input
-              id="title"
-              className="form-control"
-              name="title"
-              placeholder="nazwa listy"
-              onChange={(e) => setList_name(e.target.value)}
-              value={list_name}
-            />
-          </div>
-          <div>
-            <button
-              onClick={handleSubmitListName}
-              id="submit"
-              className="btn btn-warning"
-              type="submit"
-              name="add"
-            />
-          </div>
-
-        </form>
-        <form onSubmit={handleSubmitTask} id="form">
-          <div>
-            <input
-              id="title"
-              className="form-control"
-              name="title"
-              placeholder="nazwa taska"
-              onChange={handleChange}
-              value={activeItem.title}
-            />
-            <input
-              className="form-control"
-              placeholder="id listy"
-              onChange={handleChangeListID}
-              value={activeItem.list}
-            />
-            <input
-              id="submit"
-              className="form-control"
-              type="submit"
-              name="add"
-            />
-          </div>
-        </form>
-      </div>
-
-    )
-  }
 
   return (
     <div id="mainEditorComponent" className="root col-5">
       <div className="scrollIt">
         <div id="centerComponent" className="row">
-          <div onClick={() => toggleTrueFalse()} className="">
+          <div onClick={() => toggleTrueFalse()} className="col-2">
             <FontAwesomeIcon icon={faPlusSquare}/>
           </div>
-          {isToggled ? showForm() : <div> Add new list or choose existing one to edit </div>}
+          {isToggled ? showListForm() : <div> Add new list or choose existing one to edit </div>}
           {list ? renderClickedList(list) : null}
         </div>
       </div>
